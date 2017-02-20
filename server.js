6ec6f94fa2792599261b65a48b9cabe36f89488b
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-02-20 21:49:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article_one', function (req, res) {
-  res.sendFile('Article-one requested and will be served here');
+app.get('/article-one', function (req, res) {
+  res.send('Article-one requested and will be served here');
 });
 
 app.get('/article_two', function (req, res) {
-  res.sendFile('Article-two requested and will be served here');
+  res.send("Article-two requested and will be served here");
 });
 
 app.get('/article_three', function (req, res) {
-  res.sendFile('Article-three requested and will be served here');
+  res.send('Article-three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
