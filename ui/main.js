67dad94b1e704b8b0b642fafc3c5dc5d285cfe06
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-02-23 23:01:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 var element=document.getElementById("main-text");
 var img=document.getElementById("madi");
+var leftmargin=0;
+function moveright(){
+    leftmargin=leftmargin+5;
+}
 img.onclick=function(){
-    element.innerHTML="New value";
+    var interval=setInterval(moveright,50);
+    element.innerHTML="You clicked the image!";
 }
