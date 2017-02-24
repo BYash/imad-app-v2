5ebfab1a548636caedd378905aaf1c9786c78ef5
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-02-24 21:57:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,7 @@
 var button=document.getElementById('counter');
-button.onclick=function(){
-	var request=new XMLHttpRequest();
-	request.onreadystatechange=function (){
-		if(request.readyState===XMLHttpRequest.DONE){
-			if(request.status===200){
-				var counter=request.reponseText;
-				var span=document.getElementById('count');
-				span.innerHTML=counter.toString();
-			}
-		}
-	};
-	request.open('GET','http://byash.imad.hasura-app.io/counter',true);
-	request.send(null);
+var counter=0;
+button.onclick=function (){
+counter=counter+1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
 };
