24d51f620e0ad7dec9959268cbdfe93fd653d987
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-02-28 20:30:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var config = {
     password: process.env.DB_PASSWORD
 };
 
-var pool = new Pool(config);
+var pool = new pool(config);
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
        if(err){
