5ba95b86db5da294b0ba6590c9efe7bab1925fdf
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-03-01 14:48:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,11 +77,11 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-app.get('/article_one', function (req, res) {
+app.get('/article_one/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_one.html'));
 });
 
-app.get('/article_two', function (req, res) {
+app.get('/article_two/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article_two.html'));
 });
 
