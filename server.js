fbc8558428f85c98af43e1838d5f54a07b64cb82
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-03-01 13:48:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,9 +14,51 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 
+var articles = {
+    'article-one': {
+        title: 'Article one | Yash B',
+        heading: 'Article one',
+        content: `<p>
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+                </p>
+                <p>
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+                </p>
+                <p>
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.
+                    This is the content of article one.This is the content of article one.This is the content of article one.This is the content of article one.
+                </p>`
+    },
+    'article-two': {
+        title: 'Article two | Yash B',
+        heading: 'Article two',
+        content: `
+            <p>
+            This is the content for article two.
+            </p>`
+    },
+    'article-three': {
+        title: 'Article three | Yash B',
+        heading: 'Article three',
+        content:`
+            <p>
+            This is the content of article three.
+            </p>`
+    }
+};
+
+function createTemplate (data) {
+    
+}
+
 var pool = new Pool(config);
 app.get('/test_db',function(req,res){
-    pool.query('SELECT * FROM test',function(err,result){
+    pool.query('select * from test',function(err,result){
        if(err){
            res.status(500).send(err.toString());
        } else {
@@ -45,6 +87,21 @@ app.get('/article_two', function (req, res) {
 
 app.get('/article_three', function (req, res) {
   res.send('Article-three requested and will be served here');
+});
+
+app.get('articles/:articleName',function(req,res){
+   pool.query("select * from article where title = " + req.params.articleName, function(req,res) {
+      if(err) {
+          res.status(500).send(err.toString());
+      } else {
+          if(result.rows.length === 0) {
+              res.status(404).send('Article not found');
+          } else {
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+   });
 });
 
 app.get('/ui/style.css', function (req, res) {
