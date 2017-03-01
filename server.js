9d9163d65337386df775506b6d1b386f6a0fa491
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-03-01 15:15:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var articles = {
     'article-two': {
         title: 'Article two | Yash B',
         links:  <a href="/">Home</a>
-                <a href="/article-one/">Article-two</a>
+                <a href="/article-one/">Article-one</a>
                 <a href="/article-three/">Article-three</a>,
         heading: 'Article two',
         content: `
@@ -51,8 +51,8 @@ var articles = {
     'article-three': {
         title: 'Article three | Yash B',
         links: <a href="/">Home</a>
-                <a href="/article-one/">Article-two</a>
-                <a href="/article-two/">Article-three</a>,
+                <a href="/article-one/">Article-one</a>
+                <a href="/article-two/">Article-two</a>,
         heading: 'Article three',
         content:`
             <p>
