71312a17758dfbb7d623ffd55e1db2477f52bd72
Yash Bhavsar <yash.bhavsar@somaiya.edu>
2017-03-26 01:25:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ var config = {
         port: '5432'
 };
 
+var pool = new Pool(config);
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -94,7 +96,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var pool = new Pool(config);
 app.get('/test-dtb',function(req,res){
     pool.query('select * from test',function(err,result){
        if(err){
